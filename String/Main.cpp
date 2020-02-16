d830aa0a923884fd1c05c2dcbfd6ceb58a0ac490
Francisco Samuel Martins <fsamuelsmartins@gmail.com>
2020-02-16 16:16:31 -0300

    cases prontos, == e != pronto

--- String/Main.cpp
+++ String/Main.cpp
@@ -1,8 +1,62 @@
 #include "String.cpp";
-int main() {
-	String a = 'b';
+#include <iostream>
 
-	cout << a;
+int main() {
+
+	String* a = new String();
+	String* b = new String('a');
+	String* c = new String("ab");
+
+	cout << "String: " << a << " tamanho: " << a->getLength()<< endl;
+	cout << "String: " << b << " tamanho: " << b->getLength() << endl;
+	cout << "String: " << c << " tamanho: " << c->getLength() << endl;
+
+	String d = 'a';
+	String e = "a";
+	String f = c;
+	String g = d;
+
+
+	cout << "String: " << d << " tamanho: " << d.getLength() << endl;
+	cout << "String: " << e << " tamanho: " << e.getLength() << endl;
+	cout << "String: " << f << " tamanho: " << f.getLength() << endl;
+	cout << "String: " << g << " tamanho: " << g.getLength() << endl;
+	
+	cout <<d.getIndice('b') <<   endl;
+	cout << c->getIndice('b') << endl;
+
+	String* cases = new String("SaMuel");
+	cout << cases->lowerCase(0, 5) << endl;
+	cout << cases->toggleCase(0, 5) << endl;
+	cout << cases->upperCase(0, 5) << endl;
+	
+	cout << d[0] << endl;
+
+	if (d == b) {
+		cout << "hello" << endl;
+	}
+
+	if (b == d) {
+		cout << "hello" << endl;
+	}
+
+	if (d == e) {
+		cout << "hello" << endl;
+	}
+
+	if (c != d) {
+		cout << "hello" << endl;
+	}
+
+	if (b != f) {
+		cout << "hello" << endl;
+	}
+
+	if (f != e) {
+		cout << "hello" << endl;
+	}
+
+
 
 	return 0;
 }
--- String/String.cpp
+++ String/String.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
+
 class String {
 public:
 	String() {
 		this->length = 0;
 
 		this->word = new char('\0');
-	}
-	
+	}	
+
 	String(char new_char){
 		char* aux = new char[2];
 		aux[0] = new_char;
@@ -15,71 +16,189 @@ public:
 		this->length = 1;
 		this->word = aux;
 	}
-	/*
+	
 	String(const char* new_char) {
 		this->length = CharLength(new_char);
-		this->word = new char[length + 1];
+		char* aux = new char[length + 1];
 
 		for (int i = 0; i < length; i++) {
-			this->word[i] = new_char[i];
+			aux[i] = new_char[i];
 		}
-		this->word[length] = '\0';
+		aux[length] = '\0';
+
+		this->word = aux;
 	}
 
-	void print() {
-		cout << word << endl;
+	String(String* new_char) {
+		this->length = CharLength(new_char->getWord());
+		char* aux = new char[length + 1];
+
+		for (int i = 0; i < length; i++) {
+			aux[i] = new_char->getWord()[i];
+		}
+		aux[length] = '\0';
+
+		this->word = aux;
 	}
 
-	String operator = (String word) {
-		String copyWord
+	String(String& new_char) {
+		this->length = CharLength(new_char.getWord());
+		char* aux = new char[length + 1];
+
+		for (int i = 0; i < length; i++) {
+			aux[i] = new_char.getWord()[i];
+		}
+		aux[length] = '\0';
+
+		this->word = aux;
 	}
 
-	String operator + (const char* newWord) {
-		String value = newWord;
-
-		return ConcatString(value);
+	int getLength() {
+		return this->length;
 	}
 
-	String operator + (String  newWord) {
-
-		return ConcatString(newWord);
+	const char* getWord() {
+		return this->word;
 	}
 
-	void operator += (String  newWord) {
-		String res = ConcatString(newWord);
-		this->length = res.length;
-		this->word = res.word;
+	int getIndice(char letra) {
+		for (int i = 0; i < length; i++) {
+			if (word[i] == letra) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
-	bool operator == (String otherWord) {
-		if (length != otherWord.length) {
-			return false;
-		} else {
-			for (int i = 0; i < length; i++) {
-				if (word[i] != otherWord[i]) {
-					return false;
+	char* upperCase(int i0, int i1) {
+		if ((i0 < 0) || (i1 >= length)) 
+			throw "posicao invalida";
+		
+
+		char* aux = new char[length + 1];
+
+		for (int i = 0; i < length; i++) {
+				if (i >= i0 && i <= i1) {
+					if (word[i] >= 'a' && word[i] <= 'z') {
+						aux[i] = word[i] - 32;
+					}
+					else {
+						aux[i] = word[i];
+					}
+				}
+				else {
+					aux[i] = word[i];
+				}
+	
+		}
+		aux[length] = '\0';
+
+		return aux;
+	}
+
+	char* lowerCase(int i0, int i1) {
+		if (i0 < 0 || i1 >= this->length)
+			throw "posicao invalida";
+
+		char* aux = new char[length+1];
+
+		for (int i = 0; i < length; i++) {
+			if (i >= i0 && i <= i1) {
+				if (word[i] >= 'A' && word[i] <= 'Z') {
+					aux[i] = word[i] + 32;
+				}
+				else {
+					aux[i] = word[i];
+
 				}
 			}
-			return true;
+			else {
+				aux[i] = word[i];
+			}
+
 		}
+		aux[length] = '\0';
+
+		return aux;
+
 	}
 
-	bool operator != (String otherWord) {
-		if (length != otherWord.length) {
-			return true;
+	char* toggleCase(int i0, int i1) {
+		if (i0 < 0 || i1 >= this->length) {
+			throw "posicao invalida";
 		}
-		else {
-			for (int i = 0; i < length; i++) {
-				if (word[i] != otherWord[i]) {
-					return true;
+
+		char* aux = new char[length+1];
+
+		for (int i = 0; i < length; i++) {
+			if (i >= i0 && i <= i1) {
+				if (word[i] >= 'A' && word[i] <= 'Z') {
+					aux[i] = word[i] + 32;
+				}
+				else if (word[i] >= 'a' && word[i] <= 'z') {
+					aux[i] = word[i] - 32;
+				}
+				else {
+					aux[i] = word[i];
+
 				}
 			}
-			return false;
+			else {
+				aux[i] = word[i];
+			}
+
 		}
+
+		aux[length] = '\0';
+		return aux;
+
 	}
 
+
+	char operator [] (int index) {
+		if (this->length == 0) {
+			throw "String vazia";
+		}
+		else if (index < 0 || index >= this->length) {
+			throw "posicao nao valida";
+		}
+		return word[index];
+	}
+
+
+
+	friend auto operator == (String* word1, String& word2)->bool {
+		String word = word1->getWord();
+		return word.equalsComparation(word2);
+	}
+
+	friend auto operator == (String& word1, String* word2) ->bool {
+		String word = word2->getWord();
+		return word1.equalsComparation(word);
+	}
+
+	friend auto operator == (String& word1, String& word2) ->bool {
+		return word1.equalsComparation(word2);
+	}
+
+	friend auto operator != (String* word1, String& word2)->bool {
+		String word = word1->getWord();
+		return !word.equalsComparation(word2);
+	}
+
+	friend auto operator != (String& word1, String* word2) ->bool {
+		String word = word2->getWord();
+		return !word1.equalsComparation(word);
+	}
+
+	friend auto operator != (String& word1, String& word2) ->bool {
+		return !word1.equalsComparation(word2);
+	}
+
+
+	/*
 	bool operator > (String otherWord) {
-		int indice = otherWord.length;
+		int indice = otherWord.getLength();
 		if (indice > length) {
 			indice = length;
 		}
@@ -87,14 +206,16 @@ public:
 		for (int i = 0; i < indice; i++) {
 			if (word[i] < otherWord[i]) {
 				return false;
-			} else if (word[i] > otherWord[i]) {
+			}
+			else if (word[i] > otherWord[i]) {
 				return true;
 			}
 		}
 
-		if (length > otherWord.length) {
-			return true;
-		} else if (length < otherWord.length) {
+		if (length == otherWord.getLength()) {
+			return false;
+		}
+		else if (length < otherWord.getLength()) {
 			return false;
 		}
 
@@ -118,7 +239,8 @@ public:
 
 		if (length > otherWord.length) {
 			return false;
-		} else if (length < otherWord.length) {
+		}
+		else if (length < otherWord.length) {
 			return true;
 		}
 		return false;
@@ -133,14 +255,16 @@ public:
 		for (int i = 0; i < indice; i++) {
 			if (word[i] < otherWord[i]) {
 				return false;
-			} else if (word[i] > otherWord[i]) {
+			}
+			else if (word[i] > otherWord[i]) {
 				return true;
 			}
 		}
 
 		if (length > otherWord.length) {
 			return true;
-		} else if (length < otherWord.length) {
+		}
+		else if (length < otherWord.length) {
 			return false;
 		}
 
@@ -156,96 +280,101 @@ public:
 		for (int i = 0; i < indice; i++) {
 			if (word[i] < otherWord[i]) {
 				return true;
-			} else if (word[i] > otherWord[i]) {
+			}
+			else if (word[i] > otherWord[i]) {
 				return false;
 			}
 		}
 
 		if (length > otherWord.length) {
 			return false;
-		} else if (length < otherWord.length) {
+		}
+		else if (length < otherWord.length) {
 			return true;
 		}
 
 		return true;
 	}
 
-	char operator [] (int index) {
+*/
 
-		return word[index];
+	/*
+	String operator + (String newWord) {
+		String aux =  ConcatString(newWord);
+		return aux;
+	}
+
+
+	String operator = (String word) {
+		String copyWord
+	}
+
+	String operator + (const char* newWord) {
+		String value = newWord;
+
+		return ConcatString(value);
+	}
+
+		String operator = (String newWord) {
+		cout << "oie" << endl;
+		this->length = CharLength(newWord.getWord());
+		char* aux = new char[length + 1];
+		for (int i = 0; i < length; i++) {
+			aux[i] = newWord.getWord()[i];
+		}
+		aux[length] = '\0';
+		String* deleteAux = new String(aux);
+		return aux;
+	}
+
+	void operator += (String  newWord) {
+		String res = ConcatString(newWord);
+		this->length = res.length;
+		this->word = res.word;
+	}
+
+	
+
+
+	*/
+	friend ostream& operator << (ostream& out, String* c) {
+		out << c->getWord();
+		return out;
 	}
 
 	friend ostream& operator << (ostream& out, String& c) {
-		out << c.word;
+		out << c.getWord();
 		return out;
 	}
 
 	friend istream& operator >> (istream& in, String& c) {
-		in >> c.word;
-
+		char* charAux;
+		in >> charAux;
 		int size = 0;
 
-		String aux = c.word;
+		String aux = charAux;
 		c = aux;
 		return in;
 	}
 
-	int getLegth() {
-		return length;
+	friend istream& operator >> (istream& in, String* c) {
+		char* charAux;
+		in >> charAux;
+
+		c = new String(charAux);
+		return in;
 	}
 
-	int indice(char letra) {
-		for (int i = 0; i < length; i++) {
-			if (word[i] == letra) {
-				return i;
-			}
-		}
-		return NULL;
-
-	}
-
-	void upperCase(int i0, int i1) {
-
-		for (int i = i0; i <= i1; i++) {
-			if (word[i] >= 'a' && word[i] <= 'z') {
-				word[i] = word[i] - 32;
-			}
-		}
-	}
-
-	void lowerCase(int i0, int i1) {
-		for (int i = i0; i <= i1; i++) {
-			if (word[i] >= 'A' && word[i] <= 'Z') {
-				word[i] = word[i] + 32;
-			}
-		}
-	}
-
-	void toggleCase(int i0, int i1) {
-		for (int i = i0; i <= i1; i++) {
-			if (word[i] >= 'A' && word[i] <= 'Z') {
-				word[i] = word[i] + 32;
-			}
-			else if (word[i] >= 'a' && word[i] <= 'z') {
-				word[i] = word[i] - 32;
-			}
-		}
-	}
-	*/
-	friend ostream& operator << (ostream& out, String& c) {
-		out << c.word;
-		return out;
-	}
 
 	~String() {
-
+		delete(word);
 	}
 
 private:
 	const char* word;
 	int length;
 
-	String ConcatString(String secondString) {
+	String ConcatString(String& secondString) {
 		String res;
 		res.length = length + secondString.length;
 		char* newWord = new char[res.length + 1];
@@ -264,7 +393,6 @@ private:
 
 
 		return res;
-
 	}
 
 	int CharLength(const char* stringValue) {
@@ -275,5 +403,35 @@ private:
 
 		return size;
 	}
+	bool equalsComparation(String word2) {
+		if (length != word2.getLength()) {
+			return false;
+		}
+		else {
+			for (int i = 0; i < length; i++) {
+				if (word[i] != word2[i]) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+	bool equalsComparation(String word1, String word2) {
+		if (word1.getLength() != word2.getLength()) {
+			return false;
+		}
+		else {
+			for (int i = 0; i < word1.getLength(); i++) {
+				if (word1[i] != word2[i]) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
 
 };
+
+
